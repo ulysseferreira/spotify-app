e9277f5e3b3b9b446da8cb053a5e02c9a7509b19
ulysse <ulysse@alenvi.io>
2023-12-21 01:39:05 +0100

    can run build

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const opn = require('opn');
 
-app.use(express.static('public'));
+const server = http.createServer((req, res) => {
+  if (req.url === '/styles.css') {
+    const cssPath = path.join(__dirname, './public/styles.css');
+    
+    fs.readFile(cssPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading CSS file:', err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/') {
+    const htmlPath = path.join(__dirname, './public/index.html');
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+    fs.readFile(htmlPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading HTML file:', err);
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+  opn('http://localhost:3000');
 });
